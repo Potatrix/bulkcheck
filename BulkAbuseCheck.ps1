05d67200b9389c112c576a54ff3a9575eb57ace6
Potatrix <potatrix@gmail.com>
2021-01-14 23:37:23 -0800

    Update BulkAbuseCheck.ps1
    Put some work into this, still not working.
    
    Need to figure out how to format the request

--- BulkAbuseCheck.ps1
+++ BulkAbuseCheck.ps1
@@ -1,3 +1,34 @@
+# Check individual IP
+# https://www.abuseipdb.com/check/[IP]/json?key=[API_KEY]&days=[DAYS]
 
-# Grab IPs
-$IPs = import-csv C:\Users\Karl\Documents\ips.csv -Header "IP"
+# Check CIDR
+# https://www.abuseipdb.com/check-block/json?network=[CIDR]&key=[API_KEY]&days=[DAYS]
+
+function getCSVPath {
+    $csvPath = Read-Host -Prompt "CSV File Path"
+    return $csvPath
+}
+
+function grabIPs($csvPath) {
+    # Grab IPs
+    $IPs = import-csv C:\Users\Karl\Documents\ips.csv -Header "IP"   
+    return $IPs 
+}
+
+function grabAPIKey {
+    $APIKeyPath = ".\APIKey.txt"
+    # $APIKeyPath = read-host -prompt "API Key Path"
+
+    $APIKey = Get-Content -Path $APIKeyPath
+    return $APIKey
+}
+
+$csvPath = getCSVPath
+
+$APIKey = grabAPIKey
+
+
+ForEach ($IP in $IPs) {
+    $json = Invoke-WebRequest -Uri "https://www.abuseipdb.com/check/$IP/json?key=$APIKey&days=30" -ContentType "application/json" -Method Get
+}
+Write-Host($json)
